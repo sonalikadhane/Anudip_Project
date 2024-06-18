62ae54c3d3cc6fddc98c83ed21acf5cbb78d77fe
sonali kadhane <150772875+sonalikadhane@users.noreply.github.com>
2024-06-18 13:23:26 +0530

    Delete LibraryManagmentSystem directory

--- LibraryManagmentSystem/src/main/java/com/lms/repository/AdminInfoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.lms.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.lms.entities.AdminInfo;
-
-public interface AdminInfoRepository extends JpaRepository<AdminInfo,Integer>{
-
-}
